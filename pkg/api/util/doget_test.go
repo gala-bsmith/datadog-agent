1a52727a44d81067487579e8e1cef60d209a59f2
Maxime Riaud <maxime.riaud@datadoghq.com>
2024-10-16 18:30:06 +0200

    fixing missing migrated code

--- pkg/api/util/doget_test.go
+++ pkg/api/util/doget_test.go
@@ -118,7 +118,7 @@ func TestResolver(t *testing.T) {
 		cfg := model.NewConfig("datadog", "test", strings.NewReplacer("_", "."))
 		cfg.SetWithoutSource("cmd_port", server.Listener.Addr().String())
 
-		client := GetClient().WithNoVerify().WithResolver().Build()
+		client := GetClient(WithNoVerify())
 
 		data, err := DoGet(client, fmt.Sprintf("http://%v", CoreCmd), CloseConnection)
 		require.NoError(t, err)
@@ -126,7 +126,7 @@ func TestResolver(t *testing.T) {
 	})
 
 	t.Run("unknown address", func(t *testing.T) {
-		client := GetClient().WithNoVerify().WithResolver().Build()
+		client := GetClient(WithNoVerify())
 
 		handler := func(w http.ResponseWriter, _ *http.Request) {
 			w.Write([]byte("test"))
