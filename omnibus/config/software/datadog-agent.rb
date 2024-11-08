6a1aa1e9108aded793d563c535d972c0d4267269
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-11-08 07:39:54 +0100

    fix syntax

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -144,8 +144,8 @@ build do
   end
 
   # System-probe
-  sysprobe_support = (not heroku_target?) && (linux_target? || (windows_target? && do_windows_sysprobe != ""))
-                      && ENV.has_key?('SYSTEM_PROBE_BIN') and not ENV['SYSTEM_PROBE_BIN'].empty?
+  sysprobe_support = (not heroku_target?) && (linux_target? || (windows_target? && do_windows_sysprobe != "")) &&
+                       ENV.has_key?('SYSTEM_PROBE_BIN') and not ENV['SYSTEM_PROBE_BIN'].empty?
   if sysprobe_support
     if not bundled_agents.include? "system-probe"
       if windows_target?
