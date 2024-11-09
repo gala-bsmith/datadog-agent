a4c54aebe455a0d8780aceb5ed03fd2a4055a406
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-11-09 07:49:37 +0100

    init component

---
+++ comp/haagent/mock/mock.go
@@ -0,0 +1,21 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+//go:build test
+
+// Package mock provides a mock for the haagent component
+package mock
+
+import (
+	"testing"
+
+	haagent "github.com/DataDog/datadog-agent/comp/haagent/def"
+)
+
+// Mock returns a mock for haagent component.
+func Mock(t *testing.T) haagent.Component {
+	// TODO: Implement the haagent mock
+	return nil
+}
